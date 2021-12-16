a535488a06cab84eff12ba5b48e56be410627a2b
Динис Коновалов <kdinisv@gmail.com>
2021-12-17 00:23:15 +0300

    Перешел на got

--- example/test.js
+++ example/test.js
@@ -10,26 +10,28 @@ console.log("My balance:", price);
 
 // const buff = await readFile("./example/captcha.png");
 
-const { id, result } = await recognize
-  .solvingRecaptcha3(
-    "https://www.reestr-zalogov.ru/search/index",
-    "6LdKJhMaAAAAAIfeHC6FZc-UVfzDQpiOjaJUWoxr",
-    "search_notary",
-    "0.3"
-  )
-  .catch(console.error);
+// const { id, result } = await recognize
+//   .solvingRecaptcha3(
+//     "https://www.reestr-zalogov.ru/search/index",
+//     "6LdKJhMaAAAAAIfeHC6FZc-UVfzDQpiOjaJUWoxr",
+//     "search_notary",
+//     "0.3"
+//   )
+//   .catch(console.error);
 
-console.log(result);
+// console.log(id, result);
 
-// const r = await recognize.reportGood(id).catch((err) => err.message);
+// console.log(await recognize.solvingImage(buff));
+
+// const r = await recognize.reportGood(1).catch((err) => err.message);
 // console.log(r);
-// recognize.solving(data, function (err, id, code) {
-//     if (err) throw err;
-//     if (code) console.log("Captcha:", code);
-//     else {
-//       console.log("Captcha not valid");
-//       recognize.report(id, function (err, answer) {
-//         console.log(answer);
-//       });
-//     }
-//   });
+// recognize.solving(buff, function (err, id, code) {
+//   if (err) throw err;
+//   if (code) console.log("Captcha:", code);
+//   else {
+//     console.log("Captcha not valid");
+//     recognize.report(id, function (err, answer) {
+//       console.log(answer);
+//     });
+//   }
+// });
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { setTimeout } from "node:timers/promises";
-import http from "node:http";
+import got from "got";
 
 export const SOURCE = {
   RUCAPTCHA: "RUCAPTCHA",
@@ -9,15 +9,15 @@ export const SOURCE = {
 
 const _SOURCE = {
   [SOURCE.RUCAPTCHA]: {
-    baseUrl: "rucaptcha.com",
+    baseUrl: "http://rucaptcha.com",
     soft_id: 768,
   },
   [SOURCE.ANTIGATE]: {
-    baseUrl: "anti-captcha.com",
+    baseUrl: "http://anti-captcha.com",
     soft_id: 720,
   },
   [SOURCE.CAPTCHA24]: {
-    baseUrl: "captcha24.com",
+    baseUrl: "http://captcha24.com",
     soft_id: 921,
   },
 };
@@ -27,145 +27,79 @@ export default class Recognize {
     if (!options || !options.key) throw new Error("not key");
     const source = _SOURCE[sourceCode];
     if (!source) throw new Error("invalid type");
-
+    this.rp = got.extend({
+      prefixUrl: source.baseUrl,
+      resolveBodyOnly: true,
+      responseType: "json",
+    });
     this._options = {
       ...options,
-      baseUrl: source.baseUrl,
       soft_id: source.soft_id,
     };
   }
 
-  balanse() {
-    return new Promise((resolve, reject) => {
-      http
-        .get(
-          {
-            hostname: this._options.baseUrl,
-            port: 80,
-            path: `/res.php?key=${this._options.key}&action=getbalance&json=1`,
-          },
-          function (res) {
-            var body = [];
-            res.on("data", function (chunk) {
-              body.push(chunk);
-            });
-            res.on("end", function () {
-              try {
-                const { status, request } = JSON.parse(body.join(""));
-                if (status === 1) {
-                  return resolve(request);
-                }
-                return reject(new Error(request));
-              } catch (err) {
-                reject(err);
-              }
-            });
-          }
-        )
-        .on("error", reject);
+  async _waitResult(id) {
+    await setTimeout(1000);
+    const { status, request } = await this.rp.get("res.php", {
+      searchParams: { key: this._options.key, action: "get", id, json: 1 },
     });
+
+    if (request === "CAPCHA_NOT_READY") return this._waitResult(id);
+    else if (status === 0) return Promise.reject(request);
+
+    return request;
   }
 
-  solvingImage(buff, options = {}) {
-    return new Promise((resolve, reject) => {
-      const postData = JSON.stringify({
+  async balanse() {
+    const { request } = await this.rp.get("res.php", {
+      searchParams: { key: this._options.key, action: "getbalance", json: 1 },
+    });
+
+    return request;
+  }
+
+  async solvingImage(buff, options = {}) {
+    const { status, request } = await this.rp.post("in.php", {
+      json: {
         ...options,
         soft_id: this._options.soft_id,
         method: "base64",
         key: this._options.key,
         body: buff.toString("base64"),
-        json: "1",
-      });
-
-      const req = http.request(
-        {
-          hostname: this._options.baseUrl,
-          port: 80,
-          path: "/in.php",
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(postData),
-          },
-        },
-        (res) => {
-          const body = [];
-          res.setEncoding("utf8");
-          res.on("data", (chunk) => {
-            body.push(chunk);
-          });
-          res.on("end", () => {
-            const { status, request } = JSON.parse(body.join(""));
-            // console.log(request);
-            if (status === 1) {
-              waitResult(this._options.baseUrl, this._options.key, request)
-                .then((result) => resolve({ id: request, result }))
-                .catch((error) => reject(error));
-            } else {
-              reject(new Error(request));
-            }
-          });
-        }
-      );
-
-      req.on("error", reject);
-      req.write(postData);
-      req.end();
+        json: 1,
+      },
     });
+
+    if (status === 1) {
+      return { result: await this._waitResult(request), id: request };
+    }
+
+    return Promise.reject(request);
   }
 
-  solvingRecaptcha2(url, googleKey, options = {}) {
-    return new Promise((resolve, reject) => {
-      const postData = JSON.stringify({
+  async solvingRecaptcha2(url, googleKey, options = {}) {
+    const { status, request } = await this.rp.post("in.php", {
+      json: {
         ...options,
         soft_id: this._options.soft_id,
         method: "userrecaptcha",
         key: this._options.key,
         pageurl: url,
         googlekey: googleKey,
-        json: "1",
-      });
-
-      const req = http.request(
-        {
-          hostname: this._options.baseUrl,
-          port: 80,
-          path: "/in.php",
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(postData),
-          },
-        },
-        (res) => {
-          const body = [];
-          res.setEncoding("utf8");
-          res.on("data", (chunk) => {
-            body.push(chunk);
-          });
-          res.on("end", () => {
-            const { status, request } = JSON.parse(body.join(""));
-            // console.log(request);
-            if (status === 1) {
-              waitResult(this._options.baseUrl, this._options.key, request)
-                .then((result) => resolve({ id: request, result }))
-                .catch((error) => reject(error));
-            } else {
-              reject(new Error(request));
-            }
-          });
-        }
-      );
-
-      req.on("error", reject);
-      req.write(postData);
-      req.end();
+        json: 1,
+      },
     });
+
+    if (status === 1) {
+      return { result: await this._waitResult(request), id: request };
+    }
+
+    return Promise.reject(request);
   }
 
-  solvingRecaptcha3(url, googleKey, action, score = "0.3", options = {}) {
-    return new Promise((resolve, reject) => {
-      const postData = JSON.stringify({
+  async solvingRecaptcha3(url, googleKey, action, score = "0.3", options = {}) {
+    const { status, request } = await this.rp.post("in.php", {
+      json: {
         ...options,
         soft_id: this._options.soft_id,
         method: "userrecaptcha",
@@ -175,126 +109,42 @@ export default class Recognize {
         key: this._options.key,
         pageurl: url,
         googlekey: googleKey,
-        json: "1",
-      });
-
-      const req = http.request(
-        {
-          hostname: this._options.baseUrl,
-          port: 80,
-          path: "/in.php",
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(postData),
-          },
-        },
-        (res) => {
-          const body = [];
-          res.setEncoding("utf8");
-          res.on("data", (chunk) => {
-            body.push(chunk);
-          });
-          res.on("end", () => {
-            const { status, request } = JSON.parse(body.join(""));
-            // console.log(request);
-            if (status === 1) {
-              waitResult(this._options.baseUrl, this._options.key, request)
-                .then((result) => resolve({ id: request, result }))
-                .catch((error) => reject(error));
-            } else {
-              reject(new Error(request));
-            }
-          });
-        }
-      );
-
-      req.on("error", reject);
-      req.write(postData);
-      req.end();
+        json: 1,
+      },
     });
+
+    if (status === 1) {
+      return { result: await this._waitResult(request), id: request };
+    }
+
+    return Promise.reject(request);
   }
 
-  reportBad(id) {
-    return new Promise((resolve, reject) => {
-      http
-        .get(
-          {
-            hostname: this._options.baseUrl,
-            port: 80,
-            path: `/res.php?key=${this._options.key}&action=reportbad&id=${id}&json=1`,
-          },
-          function (res) {
-            const body = [];
-            res.setEncoding("utf8");
-            res.on("data", function (chunk) {
-              body.push(chunk);
-            });
-            res.on("end", function () {
-              const { status, request } = JSON.parse(body.join(""));
-              if (status === 0) return reject(new Error(request));
-              resolve(request);
-            });
-          }
-        )
-        .on("error", reject);
+  async reportBad(id) {
+    const { status, request } = await this.rp.get("res.php", {
+      searchParams: {
+        key: this._options.key,
+        action: "reportbad",
+        id,
+        json: 1,
+      },
     });
+
+    if (status === 0) return false;
+    return true;
   }
 
-  reportGood(id) {
-    return new Promise((resolve, reject) => {
-      http
-        .get(
-          {
-            hostname: this._options.baseUrl,
-            port: 80,
-            path: `/res.php?key=${this._options.key}&action=reportgood&id=${id}&json=1`,
-          },
-          function (res) {
-            const body = [];
-            res.setEncoding("utf8");
-            res.on("data", function (chunk) {
-              body.push(chunk);
-            });
-            res.on("end", function () {
-              const { status, request } = JSON.parse(body.join(""));
-              if (status === 0) return reject(new Error(request));
-              resolve(request);
-            });
-          }
-        )
-        .on("error", reject);
+  async reportGood(id) {
+    const { status, request } = await this.rp.get("res.php", {
+      searchParams: {
+        key: this._options.key,
+        action: "reportgood",
+        id,
+        json: 1,
+      },
     });
+
+    if (status === 0) return false;
+    return true;
   }
 }
-
-const httpGet = (request) => {
-  return new Promise((resolve, reject) => {
-    return http
-      .get(request, (res) => {
-        const body = [];
-        res.setEncoding("utf8");
-        res.on("data", function (chunk) {
-          body.push(chunk);
-        });
-        res.on("end", () => {
-          resolve(JSON.parse(body.join("")));
-        });
-      })
-      .on("error", reject);
-  });
-};
-
-const waitResult = async (baseUrl, key, id) => {
-  await setTimeout(1000);
-  const { status, request } = await httpGet({
-    hostname: baseUrl,
-    port: 80,
-    path: `/res.php?key=${key}&action=get&id=${id}&json=1`,
-  });
-
-  if (request === "CAPCHA_NOT_READY") return waitResult(baseUrl, key, id);
-  else if (status === 0) throw new Error(request);
-
-  return request;
-};
